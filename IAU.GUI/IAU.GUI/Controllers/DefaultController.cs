b2375faecf4eb88189250f3c954d659d6fd443d8
unknown <behnamm92@gmail.com>
2020-10-15 11:55:59 -0700

    add api

---
+++ IAU.GUI/IAU.GUI/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAU.GUI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("hello/{name}")]
+        public string SayHello([FromBody]string name)
+        {
+            return "hello " + name;
+        }
+    }
+}
